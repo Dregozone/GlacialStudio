2618ddcf80d637fc1de6510781ba7408d07da6f4
Dregozone <aclearmonth@gmail.com>
2020-05-02 18:08:10 +0100

    Template progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,12 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        
         <style>
             html, body {
                 background-color: #fff;
@@ -62,26 +68,44 @@
                 margin-bottom: 30px;
             }
             
+            a {
+                text-decoration: none !important;
+            }
             
             header { 
                 grid-area: header; 
-                display: flex; 
+                display: flex;
+                flex-direction: row;
                 justify-content: center; 
-                align-content: space-between; 
-                height: 10vmin;
-                background: lightblue;
+                align-content: space-between;
+                background: #adcce6;
             }
                 header > .logo {
                     width: 25%;
+                    padding: 0.5%;
                 }
-                img.gsLogo {
-                    /*  */
-                }
+                    img.gsLogoSmall {
+                        width: 10%;
+                        margin-left: 2%;
+                    }
+                    img.gsLogo {
+                        width: 60%;
+                    }
                 header > nav {
                     width: 50%;
+                    text-align: center;
+                    color: darkslategrey !important;
                 }
+                    header > nav > a {
+                        color: darkslategrey !important;
+                    }
+                    header > nav > a:hover {
+                        color: black !important;
+                    }
                 header > .socialMedia {
+                    display: block;
                     width: 25%;
+                    text-align: center;
                 }
             
             main { grid-area: main; }
@@ -92,23 +116,29 @@
             }
                 footer > .links {
                     display: flex;
+                    flex-direction: row;
                     justify-content: center;
                     align-content: space-around;
-                    height: 10vmin;
-                    background: lightblue;
+                    background: #adcce6;
                     text-align: center;
+                    padding: 2vmin;
                 }
-                    footer > .links > * {
+                    footer > .links > .linksLeft, footer > .links > .linksRight {
                         width: 50%;
                     }
-                    .linksLeft > a, .linksRight > a {
+                    .linksLeft > div > a, .linksRight > div > a {
                         display: block;
+                        color: darkslategrey !important;
                     }
+                        .linksLeft > div > a:hover, .linksRight > div > a:hover {
+                            color: black !important;
+                        }
             
                 footer > .copy {
-                    height: 4vmin;
-                    background: blue;
+                    background: #95bad8;
                     text-align: center;
+                    padding: 1vmin;
+                    color: black;
                 }
             
             .grid-container {
@@ -122,7 +152,34 @@
                 /*  */
             }
             
+            /* FOR MOBILE */
+            @media only screen and (max-width: 600px) {
+                header {
+                    flex-direction: column;
+                }
+                    header > .logo {
+                        width: 100%;
+                        text-align: center;
+                    }
+                    header > nav {
+                        width: 100%;
+                    }
+                    header > .socialMedia {
+                        display: none;
+                    }
+                
+                footer > .links {
+                    flex-direction: column;
+                }
+                    footer > .links > .linksLeft, footer > .links > .linksRight {
+                        width: 100%;
+                    }
+            }
+            
         </style>
+        
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </head>
     <body>
             
@@ -131,7 +188,8 @@
             <header>
                 <section class="logo">
                     <a href="home">
-                        <img src="" class="gsLogo" alt="Glacial Studio Logo" aria-label="Glacial Studio Logo" />
+                        <img src={{ asset('images/Logo-small.png') }} class="gsLogoSmall" alt="Glacial Studio Logo" aria-label="Glacial Studio Logo" />
+                        <img src={{ asset('images/Logo.png') }} class="gsLogo" alt="Glacial Studio Logo" aria-label="Glacial Studio Logo" />
                     </a>
                 </section>
                 
@@ -153,20 +211,20 @@
                 
                 <section class="links">
                     <section class="linksLeft">
-                        <a href="about">About</a>
-                        <a href="contact">Contact</a>
-                        <a href="news">News</a>
+                        <div><a href="about">About</a></div>
+                        <div><a href="contact">Contact</a></div>
+                        <div><a href="news">News</a></div>
                     </section>
 
                     <section class="linksRight">
-                        <a href="termsofservice">Terms of service</a>
-                        <a href="privacypolicy">Privacy policy</a>
-                        <a href="">Site map</a>
+                        <div><a href="termsofservice">Terms of service</a></div>
+                        <div><a href="privacypolicy">Privacy policy</a></div>
+                        <div><a href="">Site map</a></div>
                     </section>
                 </section>
                 
                 <section class="copy">
-                    &copy; Copyright 2020
+                    &copy; Copyright <?= date('Y'); ?>
                 </section>
             </footer>
         </div>

c35dd64bdbd87081466ec58907d43f4e8c84381d
Debian <bitnami@ip-172-26-6-160.eu-west-2.compute.internal>
2020-09-11 22:05:10 +0000

    Minor: Static IP

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'GlacialStudio'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://18.132.97.201'),
+    'url' => env('APP_URL', 'http://18.134.40.25'),
 
     'asset_url' => env('ASSET_URL', null),
 

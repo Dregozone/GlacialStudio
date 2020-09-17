9b48d6569b9cd077992159671ae8533bfb37572a
GlacialStudio <aclearmonth@gmail.com>
2020-09-17 09:12:00 +0100

    About change

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,27 +3,27 @@
     <head>
         <!-- Project level <head> information -->
         @include('includes.htmlHead')
-        
+
         <!-- Page level <head> information -->
-        <title>Home - Glacial Studio</title>
-        
-        
+        <title>About - Glacial Studio</title>
+
+
     </head>
     <body>
         <!-- Show header bar including logo, nav and social media -->
         @include('includes.header')
-            
+
         <!-- Start dynamic page content -->
         <main>
             <h1 class="heading">About</h1>
-            
+
             <p class="center">
                 Coming soon...
             </p>
-            
+
         </main>
         <!-- End dynamic page content -->
-            
+
         <!-- Show footer bars including quicklinks and copyright -->
         @include('includes.footer')
     </body>

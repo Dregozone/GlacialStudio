5ae05e068b2b94e6dd91cbf07ff64c740dc02bb3
GlacialStudio <aclearmonth@gmail.com>
2022-01-27 16:00:34 +0000

    Adds content to about and portfolio with images. Content now copied across, needs email functionality and footer links then release.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,5 +5,77 @@
 @endsection
 
 @section('content')
-    About content 
+    <section>
+        <div>
+            <h1>About</h1>
+            
+            
+
+
+
+            <section class="centerContent aboutPara">
+                <p>
+                    Using a range of the latest technologies and taking a streamlined approach to development with focus on fast,
+                    efficient design, I aim to provide sleek web applications with a unique eye for user-experience that will leave your
+                    clients fulfilled.
+                </p>
+            </section>
+
+            <section class="centerContent">
+                <ul style="flex-direction: column; width: 100%;">
+                    <li> Effective solutions </li>
+                    <li> Affordable pricing </li>
+                    <li> Striving for customer satisfaction </li>
+                    <li> Clear timeframes for delivery </li>
+                </ul>
+            </section>
+
+            <div class="aboutSplitter">
+                <div class="technologyTable">
+                    <h3>Technologies used:</h3>
+
+                    <table class="technologyTable">
+                        <tr>
+                            <td class="techLogo"> <img class="techLogo" src="{{ asset('img/technologies/php.jpg') }}" alt="PHP Logo" /> </td>
+                            <td class="techName">PHP</td>
+                            <td class="techLogo"> <img class="techLogo" src="{{ asset('img/technologies/aws.jpg') }}" alt="AWS Logo" /> </td>
+                            <td class="techName">AWS</td>
+                        </tr>
+                        <tr>
+                            <td class="techLogo"> <img class="techLogo" src="{{ asset('img/technologies/laravel.jpg') }}" alt="Laravel Logo" /> </td>
+                            <td class="techName">Laravel</td>
+                            <td class="techLogo"> <img class="techLogo" src="{{ asset('img/technologies/react.jpg') }}" alt="React Logo" /> </td>
+                            <td class="techName">React</td>
+                        </tr>
+                        <tr>
+                            <td class="techLogo"> <img class="techLogo" src="{{ asset('img/technologies/javascript.jpg') }}" alt="Javascript Logo" /> </td>
+                            <td class="techName">Javascript</td>
+                            <td class="techLogo"> <img class="techLogo" src="{{ asset('img/technologies/bootstrap.jpg') }}" alt="Bootstrap Logo" /> </td>
+                            <td class="techName">Bootstrap</td>
+                        </tr>
+                    </table>
+                </div>
+
+                <div class="technologyTable">
+                    <div class="card" style="max-width: 15rem; margin: auto;">
+                        <img class="card-img-top" src="{{ asset('img/Anders.jpg') }}" alt="Photo of Anders">
+
+                        <div class="card-body" style="padding: 2%;">
+                            <h5 class="card-title" style="padding: 0; margin: 0; text-align: center;">Anders Learmonth MSc.</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+        </div>
+    </section>
 @endsection
--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -5,5 +5,43 @@
 @endsection
 
 @section('content')
-    Portfolio content 
+    <section>
+        <div style="width: 70%; margin-left: 15%;">
+            <h1>Portfolio</h1>
+            
+            <div class="projects">
+                <div class="project">
+                    <img src="{{ asset('img/portfolio/adscreens.jpg') }}" alt="" />
+                </div>
+
+                <div class="project">
+                    <img src="{{ asset('img/portfolio/FaceFacts.jpg') }}" alt="" />
+                </div>
+
+                <div class="project">
+                    <img src="{{ asset('img/portfolio/i-walk.jpg') }}" alt="" />
+                </div>
+
+                <div class="project">
+                    <img src="{{ asset('img/portfolio/LittleMooreLighting.jpg') }}" alt="" />
+                </div>
+
+                <div class="project">
+                    <img src="{{ asset('img/portfolio/HolisticVision.jpg') }}" alt="" />
+                </div>
+
+                <div class="project">
+                    <img src="{{ asset('img/portfolio/BlingWing.jpg') }}" alt="" />
+                </div>
+
+                <div class="project">
+                    <img src="{{ asset('img/portfolio/ExclusionZone.jpg') }}" alt="" />
+                </div>
+
+                <div class="project">
+                    <img src="{{ asset('img/portfolio/EnergizedGaming.jpg') }}" alt="" />
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection

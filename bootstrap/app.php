9ed7b5f0e308646928fa07ec63ccd230509a5aa0
Anders <aclearmonth@gmail.com>
2026-04-21 18:26:00 +0100

    Sets up rate limiting, max 20 hits per IP address per minute to try and reduce this weird traffic. Updates robots.txt to include google bot.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
+use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Middleware\ThrottleRequests;
+use Illuminate\Support\Facades\RateLimiter;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,8 +14,20 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    // ->withMiddleware(function (Middleware $middleware) {
+    //     //
+    // })
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        RateLimiter::for('web-routes', function (Request $request) {
+            return Limit::perMinute(20)->by($request->ip())
+                ->response(function (Request $request, array $headers) {
+                    return response('Too many requests. Please slow down.', 429, $headers);
+                });
+        });
+
+        $middleware->web(append: [
+            ThrottleRequests::class.':web-routes',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

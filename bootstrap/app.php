c2b3064b4b0929568c49ef3dd144d49b4a80a260
Anders <aclearmonth@gmail.com>
2026-04-21 18:28:30 +0100

    fix: moves rate limiter setup to AppServiceProvider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,9 @@
 <?php
 
-use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Middleware\ThrottleRequests;
-use Illuminate\Support\Facades\RateLimiter;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,13 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
     //     //
     // })
     ->withMiddleware(function (Middleware $middleware) {
-        RateLimiter::for('web-routes', function (Request $request) {
-            return Limit::perMinute(20)->by($request->ip())
-                ->response(function (Request $request, array $headers) {
-                    return response('Too many requests. Please slow down.', 429, $headers);
-                });
-        });
-
         $middleware->web(append: [
             ThrottleRequests::class.':web-routes',
         ]);
